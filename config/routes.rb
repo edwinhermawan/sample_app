e8eddcdabe992868e49b2b14a5b46abdc2b18403
Edwin Hermawan <edwin@signaturepages.com>
2011-12-01 17:49:07 -0500

    Added a sessions controller and a sign in view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 SampleApp::Application.routes.draw do
   
+  resources :sessions, :only => [:new, :create, :destroy]
   resources :users
 
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
-  
+  match '/signin', :to => 'sessions#new'
   match '/signup', :to => 'users#new'
+  match '/signout', :to => 'sessions#destroy'
   root :to => "pages#home"
 
 
