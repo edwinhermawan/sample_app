cbf6290409a2261003f464ac208a411dceb631c9
Edwin Hermawan <edwin@signaturepages.com>
2011-12-06 17:41:27 -0500

    Friendly forwarding and correct user requirement

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Edwin"
+  user.email                 "edwin@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
